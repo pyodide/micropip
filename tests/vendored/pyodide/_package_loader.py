f579f664cf5c352ab275a656c515febe8c4754c6
Roman Yurchak <rth.yurchak@gmail.com>
2022-09-17 12:40:25 +0200

    Vendor pyodide and _pyodide packages

--- tests/vendored/pyodide/_package_loader.py
+++ tests/vendored/pyodide/_package_loader.py
@@ -10,8 +10,7 @@ from importlib.metadata import Distribution
 from importlib.metadata import distributions as importlib_distributions
 from pathlib import Path
 from site import getsitepackages
-from tempfile import NamedTemporaryFile
-from typing import IO, Any, Literal
+from typing import IO, Any
 from zipfile import ZipFile
 
 try:
@@ -19,7 +18,7 @@ try:
 except ImportError:
     loadedPackages = None
 
-from ._core import IN_BROWSER, JsProxy, to_js
+# from ._core import IN_BROWSER, JsProxy, to_js
 
 SITE_PACKAGES = Path(getsitepackages()[0])
 STD_LIB = Path(sysconfig.get_path("stdlib"))
@@ -36,16 +35,6 @@ EXTENSION_TAGS = [suffix.removesuffix(".so") for suffix in EXTENSION_SUFFIXES]
 PLATFORM_TAG_REGEX = re.compile(
     r"\.(cpython|pypy|jython)-[0-9]{2,}[a-z]*(-[a-z0-9_-]*)?"
 )
-
-
-def parse_wheel_name(filename: str) -> tuple[str, str, str, str, str]:
-    tokens = filename.split("-")
-    # TODO: support optional build tags in the filename (cf PEP 427)
-    if len(tokens) < 5:
-        raise ValueError(f"{filename} is not a valid wheel file name.")
-    version, python_tag, abi_tag, platform = tokens[-4:]
-    name = "-".join(tokens[:-4])
-    return name, version, python_tag, abi_tag, platform
 
 
 # Vendored from packaging
@@ -103,11 +92,11 @@ def make_whlfile(
     return shutil._make_zipfile(*args, **kwargs)  # type: ignore[attr-defined]
 
 
-if IN_BROWSER:
-    shutil.register_archive_format("whl", make_whlfile, description="Wheel file")
-    shutil.register_unpack_format(
-        "whl", [".whl", ".wheel"], shutil._unpack_zipfile, description="Wheel file"  # type: ignore[attr-defined]
-    )
+# if IN_BROWSER:
+#    shutil.register_archive_format("whl", make_whlfile, description="Wheel file")
+#    shutil.register_unpack_format(
+#        "whl", [".whl", ".wheel"], shutil._unpack_zipfile, description="Wheel file"  # type: ignore[attr-defined]
+#    )
 
 
 def get_format(format: str) -> str:
@@ -119,92 +108,6 @@ def get_format(format: str) -> str:
         if "." + format in extensions:
             return fmt
     raise ValueError(f"Unrecognized format {format}")
-
-
-def unpack_buffer(
-    buffer: JsProxy,
-    *,
-    filename: str = "",
-    format: str | None = None,
-    target: Literal["site", "lib", "dynlib"] | None = None,
-    extract_dir: str | None = None,
-    calculate_dynlibs: bool = False,
-    installer: str | None = None,
-    source: str | None = None,
-) -> JsProxy | None:
-    """Used to install a package either into sitepackages or into the standard
-    library.
-
-    This is a helper method called from ``loadPackage``.
-
-    Parameters
-    ----------
-    buffer
-        A Javascript ``Uint8Array`` with the binary data for the archive.
-
-    filename
-        The name of the file we are extracting. We only care about it to figure
-        out whether the buffer represents a tar file or a zip file. Ignored if
-        format argument is present.
-
-    format
-        Controls the format that we assume the archive has. Overrides the file
-        extension of filename. In particular we decide the file format as
-        follows:
-
-        1. If format is present, we use that.
-        2. If file name is present, it should have an extension, like a.zip,
-           a.tar, etc. Then we use that.
-        3. If neither is present or the file name has no extension, we throw an
-           error.
-
-
-    extract_dir
-        Controls which directory the file is unpacked into. Default is the
-        working directory. Mutually exclusive with target.
-
-    target
-        Controls which directory the file is unpacked into. Either "site" which
-        unpacked the file into the sitepackages directory or "lib" which
-        unpacked the file into the standard library. Mutually exclusive with
-        extract_dir.
-
-    calculate_dynlibs
-        If true, will return a Javascript Array of paths to dynamic libraries
-        ('.so' files) that were in the archive. We need to precompile these Wasm
-        binaries in `load-pyodide.js`. These paths point to the unpacked
-        locations of the .so files.
-
-    Returns
-    -------
-        If calculate_dynlibs is True, a Javascript Array of dynamic libraries.
-        Otherwise, return None.
-
-    """
-    if format:
-        format = get_format(format)
-    if target and extract_dir:
-        raise ValueError("Cannot provide both 'target' and 'extract_dir'")
-    if not filename and format is None:
-        raise ValueError("At least one of filename and format must be provided")
-    if target:
-        extract_path = TARGETS[target]
-    elif extract_dir:
-        extract_path = Path(extract_dir)
-    else:
-        extract_path = Path(".")
-    filename = filename.rpartition("/")[-1]
-    with NamedTemporaryFile(suffix=filename) as f:
-        buffer._into_file(f)
-        shutil.unpack_archive(f.name, extract_path, format)
-        suffix = Path(filename).suffix
-        if suffix == ".whl":
-            set_wheel_installer(filename, f, extract_path, installer, source)
-        if calculate_dynlibs:
-            suffix = Path(f.name).suffix
-            return to_js(get_dynlibs(f, suffix, extract_path))
-        else:
-            return None
 
 
 def should_load_dynlib(path: str) -> bool:
@@ -223,54 +126,6 @@ def should_load_dynlib(path: str) -> bool:
     # `some.cpython-39-x86_64-linux-gnu.so` Let's make a best effort here to
     # check.
     return not PLATFORM_TAG_REGEX.match(tag)
-
-
-def set_wheel_installer(
-    filename: str,
-    archive: IO[bytes],
-    target_dir: Path,
-    installer: str | None,
-    source: str | None,
-) -> None:
-    """Record the installer and source of a wheel into the `dist-info`
-    directory.
-
-    We put the installer into an INSTALLER file according to the packaging spec:
-    packaging.python.org/en/latest/specifications/recording-installed-packages/#the-dist-info-directory
-
-    We put the source into PYODIDE_SORUCE.
-
-    The packaging spec allows us to make custom files. It also allows wheels to
-    include custom files in their .dist-info directory. The spec has no attempt
-    to coordinate these so that installers don't trample files that wheels
-    include. We make a best effort with our PYODIDE prefix.
-
-    Parameters
-    ----------
-    filename
-        The file name of the wheel.
-
-    archive
-        A binary representation of a wheel archive
-
-    target_dir
-        The directory the wheel is being installed into. Probably site-packages.
-
-    installer
-        The name of the installer. Currently either `pyodide.unpackArchive`,
-        `pyodide.loadPackage` or `micropip`.
-
-    source
-        Where did the package come from? Either a url, `pyodide`, or `PyPI`.
-    """
-    z = ZipFile(archive)
-    wheel_name = parse_wheel_name(filename)[0]
-    dist_info_name = wheel_dist_info_dir(z, wheel_name)
-    dist_info = target_dir / dist_info_name
-    if installer:
-        (dist_info / "INSTALLER").write_text(installer)
-    if source:
-        (dist_info / "PYODIDE_SOURCE").write_text(source)
 
 
 def get_dynlibs(archive: IO[bytes], suffix: str, target_dir: Path) -> list[str]:
--- tests/vendored/pyodide/ffi/__init__.py
+++ tests/vendored/pyodide/ffi/__init__.py
@@ -1,25 +1,2 @@
-from _pyodide._importhook import register_js_module, unregister_js_module
-
-from .._core import (
-    IN_BROWSER,
-    ConversionError,
-    JsException,
-    JsProxy,
-    create_once_callable,
-    create_proxy,
-    destroy_proxies,
-    to_js,
-)
-
-__all__ = [
-    "IN_BROWSER",
-    "ConversionError",
-    "JsException",
-    "JsProxy",
-    "create_once_callable",
-    "create_proxy",
-    "destroy_proxies",
-    "to_js",
-    "register_js_module",
-    "unregister_js_module",
-]
+def to_js(x):
+    return x
