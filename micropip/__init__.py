0eb980351ecc145c78157f989a82dd7f443fbe5c
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-11-17 15:59:32 +0000

    merged and linted

--- micropip/__init__.py
+++ micropip/__init__.py
@@ -1,9 +1,9 @@
 from ._micropip import _list as list
-from ._micropip import freeze, install
+from ._micropip import add_mock_package, freeze, install
 
 try:
     from ._version import __version__
 except ImportError:
     pass
 
-__all__ = ["install", "list", "freeze", "__version__"]
+__all__ = ["install", "list", "freeze", "add_mock_package", "__version__"]
