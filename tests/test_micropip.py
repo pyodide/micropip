0a6471adc23ad149f2a41d6ac035349cf7d26fd1
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-11-17 15:53:37 +0000

    added test

--- tests/test_micropip.py
+++ tests/test_micropip.py
@@ -198,8 +198,9 @@ def mock_fetch(monkeypatch, mock_importlib, wheel_base):
 @pytest.fixture(scope="module")
 def wheel_path(tmp_path_factory):
     # Build a micropip wheel for testing
-    import build
     from build.env import IsolatedEnvBuilder
+
+    import build
 
     output_dir = tmp_path_factory.mktemp("wheel")
 
@@ -934,3 +935,45 @@ def test_check_compatible(mock_platform, interp, abi, arch, ctx):
     wheel_name = f"{pkg}-{interp}-{abi}-{arch}.whl"
     with ctx:
         WheelInfo.from_url(wheel_name).check_compatible()
+
+
+def test_add_mock_package(monkeypatch, capsys):
+    import site
+    from importlib.metadata import version as importlib_version
+
+    from micropip._micropip import add_mock_package
+
+    with TemporaryDirectory() as tmpdirname:
+
+        def _getusersitepackages():
+            return tmpdirname
+
+        monkeypatch.setattr(site, "getusersitepackages", _getusersitepackages)
+        monkeypatch.setattr(sys, "path", [tmpdirname])
+        add_mock_package("test_1", "1.0.0")
+        add_mock_package(
+            "test_2",
+            "1.2.0",
+            modules={
+                "t1": "print('hi from t1')",
+                "t2": """
+            def fn():
+                print("Hello from fn")
+            """,
+            },
+        )
+        import t1
+
+        dir(t1)
+        import t2
+
+        dir(t2)
+        import test_1
+
+        dir(test_1)
+
+        t2.fn()
+        assert importlib_version("test_2") == "1.2.0"
+    captured = capsys.readouterr()
+    assert captured.out.find("hi from t1") != -1
+    assert captured.out.find("Hello from fn") != -1
