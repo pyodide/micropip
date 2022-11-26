67527ee8b845a32badc601632eb1bd287b083e5f
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-11-26 10:04:23 +0000

    Add add_mock_package (#26)

--- micropip/__init__.py
+++ micropip/__init__.py
@@ -1,9 +1,23 @@
 from ._micropip import _list as list
-from ._micropip import freeze, install
+from ._micropip import (
+    add_mock_package,
+    freeze,
+    install,
+    list_mock_packages,
+    remove_mock_package,
+)
 
 try:
     from ._version import __version__
 except ImportError:
     pass
 
-__all__ = ["install", "list", "freeze", "__version__"]
+__all__ = [
+    "install",
+    "list",
+    "freeze",
+    "add_mock_package",
+    "list_mock_packages",
+    "remove_mock_package",
+    "__version__",
+]
--- tests/test_micropip.py
+++ tests/test_micropip.py
@@ -934,3 +934,171 @@ def test_check_compatible(mock_platform, interp, abi, arch, ctx):
     wheel_name = f"{pkg}-{interp}-{abi}-{arch}.whl"
     with ctx:
         WheelInfo.from_url(wheel_name).check_compatible()
+
+
+@run_in_pyodide()
+def test_persistent_mock_pyodide(selenium_standalone_micropip):  #
+    import sys
+    from importlib.metadata import version as importlib_version
+    from io import StringIO
+
+    from micropip._micropip import add_mock_package
+
+    capture_stdio = StringIO()
+
+    sys.stdout = capture_stdio
+    add_mock_package("test_1", "1.0.0", persistent=True)
+    add_mock_package(
+        "test_2",
+        "1.2.0",
+        modules={
+            "t1": "print('hi from t1')",
+            "t2": """
+        def fn():
+            print("Hello from fn")
+        """,
+        },
+        persistent=True,
+    )
+    import test_1
+
+    dir(test_1)
+
+    import t1
+
+    dir(t1)
+    import t2
+
+    dir(t2)
+
+    t2.fn()
+    assert capture_stdio.getvalue().find("Hello from fn") != -1
+    assert capture_stdio.getvalue().find("hi from t1") != -1
+    assert importlib_version("test_1") == "1.0.0"
+
+
+def test_persistent_mock(monkeypatch, capsys, tmp_path):
+    import site
+    from importlib.metadata import version as importlib_version
+
+    from micropip._micropip import add_mock_package
+
+    def _getusersitepackages():
+        return tmp_path
+
+    monkeypatch.setattr(site, "getusersitepackages", _getusersitepackages)
+    monkeypatch.setattr(sys, "path", [tmp_path])
+    add_mock_package("test_1", "1.0.0")
+    add_mock_package(
+        "test_2",
+        "1.2.0",
+        modules={
+            "t1": "print('hi from t1')",
+            "t2": """
+        def fn():
+            print("Hello from fn")
+        """,
+        },
+    )
+    import t1
+
+    dir(t1)
+    import t2
+
+    dir(t2)
+    import test_1
+
+    dir(test_1)
+
+    t2.fn()
+    assert importlib_version("test_2") == "1.2.0"
+    captured = capsys.readouterr()
+    assert captured.out.find("hi from t1") != -1
+    assert captured.out.find("Hello from fn") != -1
+
+
+def test_memory_mock():
+    from micropip import _micropip
+
+    def mod_init(module):
+        module.__dict__["add2"] = lambda x: x + 2
+
+    _micropip.add_mock_package(
+        "micropip_test_bob",
+        "1.0.0",
+        modules={
+            "micropip_bob_mod": "print('hi from bob')",
+            "micropip_bob_mod.fn": mod_init,
+        },
+        persistent=False,
+    )
+    import importlib
+
+    import micropip_bob_mod
+
+    dir(micropip_bob_mod)
+
+    import micropip_bob_mod.fn
+
+    assert micropip_bob_mod.fn.add2(5) == 7
+
+    found_bob = False
+    for d in importlib.metadata.distributions():
+        if d.name == "micropip_test_bob":
+            found_bob = True
+    assert found_bob is True
+    assert (
+        importlib.metadata.distribution("micropip_test_bob").name == "micropip_test_bob"
+    )
+    # check package removes okay
+    _micropip.remove_mock_package("micropip_test_bob")
+    del micropip_bob_mod
+    with pytest.raises(ImportError):
+        import micropip_bob_mod
+
+        dir(micropip_bob_mod)
+
+
+@run_in_pyodide()
+def test_memory_mock_pyodide(selenium_standalone_micropip):
+    import pytest
+
+    from micropip import _micropip
+
+    def mod_init(module):
+        module.__dict__["add2"] = lambda x: x + 2
+
+    _micropip.add_mock_package(
+        "micropip_test_bob",
+        "1.0.0",
+        modules={
+            "micropip_bob_mod": "print('hi from bob')",
+            "micropip_bob_mod.fn": mod_init,
+        },
+        persistent=False,
+    )
+    import importlib
+
+    import micropip_bob_mod
+
+    dir(micropip_bob_mod)
+
+    import micropip_bob_mod.fn
+
+    assert micropip_bob_mod.fn.add2(5) == 7
+
+    found_bob = False
+    for d in importlib.metadata.distributions():
+        if d.name == "micropip_test_bob":
+            found_bob = True
+    assert found_bob is True
+    assert (
+        importlib.metadata.distribution("micropip_test_bob").name == "micropip_test_bob"
+    )
+    # check package removes okay
+    _micropip.remove_mock_package("micropip_test_bob")
+    del micropip_bob_mod
+    with pytest.raises(ImportError):
+        import micropip_bob_mod
+
+        dir(micropip_bob_mod)
