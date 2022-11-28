7e20cbfe3ff77f1602524d1d46b8fa03ea0a2f4a
ryanking13 <def6488@gmail.com>
2022-11-28 20:04:22 +0900

    Setup sphinx config

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+import sys
+from pathlib import Path
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "micropip"
+copyright = "2019-2022, Pyodide contributors and Mozilla"
+author = "Pyodide Authors"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+sys.path.append(Path(__file__).parent.parent.as_posix())
+
+html_theme = "sphinx_book_theme"
+html_logo = "_static/img/pyodide-logo.png"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,27 @@
+.. micropip documentation master file, created by
+   sphinx-quickstart on Mon Nov 28 19:03:15 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+micropip
+========
+
+micropip is a lightweight Python package manager for Pyodide.
+
+Overview
+----------------
+
+.. toctree::
+   :maxdepth: 1
+
+   project/api.md
+
+
+Communication
+-------------
+
+- Blog: `blog.pyodide.org <https://blog.pyodide.org/>`_
+- Mailing list: `mail.python.org/mailman3/lists/pyodide.python.org/ <https://mail.python.org/mailman3/lists/pyodide.python.org/>`_
+- Gitter: `gitter.im/pyodide/community <https://gitter.im/pyodide/community>`_
+- Twitter: `twitter.com/pyodide <https://twitter.com/pyodide>`_
+- Stack Overflow: `stackoverflow.com/questions/tagged/pyodide <https://stackoverflow.com/questions/tagged/pyodide>`_
