ea7832b07371402e07e8ee1d8be96d6ece7b6e6e
ryanking13 <def6488@gmail.com>
2023-03-10 02:16:06 +0000

    Remove loadedPackages attribute after uninstall

--- micropip/uninstall.py
+++ micropip/uninstall.py
@@ -5,11 +5,15 @@ from importlib.metadata import Distribution
 from pathlib import Path
 import shutil
 
+from ._compat import loadedPackages
 from ._importlib_helpers import _top_level_declared, _top_level_inferred
 
 
 def uninstall(packages: str | list[str]) -> None:
     """Uninstall packages.
+    
+    We do a lot simpler version of uninstallation than pip does.
+    We just remove all files that are listed in the distribution metadata.
 
     Parameters
     ----------
@@ -31,18 +35,23 @@ def uninstall(packages: str | list[str]) -> None:
     for dist in distributions:
         # Distribution._path points to .dist-info directory
         root = dist._path.parent  # type: ignore[attr-defined]
+        name = dist.name
 
         # TODO: also remove directories that are not under sitepackages directory? (e.g. data_files?)
         directories_to_remove = set(dist._path.name)  # type: ignore[attr-defined]
         directories_to_remove |= set(_top_level_declared(dist))
         directories_to_remove |= set(_top_level_inferred(dist))
 
-        try:
-            for directory in directories_to_remove:
-                path = root / directory
+        for directory in directories_to_remove:
+            path = root / directory
+            try:
                 shutil.rmtree(path, ignore_errors=False)
-        # File not found error is okay, while other errors need to be reported
-        except FileNotFoundError:
-            pass
-        finally:
-            importlib.invalidate_caches()
+            except FileNotFoundError:
+                # FileNotFoundError is okay, while other errors need to be reported
+                pass
+    
+        if hasattr(loadedPackages, name):
+            delattr(loadedPackages, name)
+        
+    importlib.invalidate_caches()
+    
