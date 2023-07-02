f9c7601cefeb8beaabfba0351210cd25b6340d1e
ryanking13 <def6488@gmail.com>
2023-07-02 22:38:26 +0900

    Add simple API and tests

---
+++ micropip/repository.py
@@ -0,0 +1,105 @@
+from dataclasses import dataclass
+from typing import Any
+
+from packaging.utils import (
+    parse_wheel_filename,
+)
+
+
+@dataclass
+class ProjectInfoFiles:
+    filename: str  # Name of the file
+    url: str  # URL to download the file
+    version: str  # Version of the package
+    sha256: str  # SHA256 hash of the file
+
+    # Size of the file in bytes, if available (PEP 700)
+    # This key is not available in the Simple API HTML response, so this field may be None
+    size: int | None = None
+
+
+@dataclass
+class ProjectInfo:
+    """
+    This class stores common metadata that can be obtained from different APIs (JSON, Simple)
+    provided by PyPI. Responses received from PyPI or other package indexes that support the
+    same APIs must be converted to this class before being processed by micropip.
+    """
+
+    name: str  # Name of the package
+
+    # List of versions available, if available (PEP 700)
+    # This key is not available in the Simple API HTML response, so this field may be None
+    versions: list[str] | None
+
+    # List of files available for the package, sorted in ascending order by version
+    # Note that a same version may have multiple files (e.g. source distribution, wheel)
+    # and this list may contain non-Pyodide compatible files (e.g. binary wheels or source distributions)
+    # so it is the responsibility of the caller to filter the list and find the best file
+    files: list[ProjectInfoFiles]
+
+    @staticmethod
+    def from_json_api(data: dict[str, Any]) -> "ProjectInfo":
+        """
+        Parse JSON API response
+
+        https://warehouse.pypa.io/api-reference/json.html
+        """
+
+        name = data["info"]["name"]
+        releases = data["releases"]
+        versions = list(releases.keys())
+
+        files = []
+        for version, fileinfo in releases.items():
+            for file in fileinfo:
+                files.append(
+                    ProjectInfoFiles(
+                        filename=file["filename"],
+                        url=file["url"],
+                        version=version,
+                        sha256=file["digests"]["sha256"],
+                        size=file["size"],
+                    )
+                )
+
+        return ProjectInfo(
+            name=name,
+            versions=versions,
+            files=files,
+        )
+
+    @staticmethod
+    def from_simple_api(data: dict[str, Any]) -> "ProjectInfo":
+        """
+        Parse Simple API response
+
+        https://peps.python.org/pep-0503/
+        https://peps.python.org/pep-0691/
+        """
+
+        name = data["name"]
+        versions = data["versions"] if "versions" in data else None
+        files = []
+        for file in data["files"]:
+            filename = file["filename"]
+            version = str(parse_wheel_filename(filename)[1])
+
+            files.append(
+                ProjectInfoFiles(
+                    filename=filename,
+                    url=file["url"],
+                    version=version,
+                    # TODO: For now we always expect that the sha256 hash is available.
+                    # This is true for PyPI, but may not be true for other package indexes,
+                    # since it is not a hard requirement of PEP503.
+                    sha256=file["hashes"]["sha256"],
+                    size=file["size"] if "size" in file else None,
+                )
+            )
+
+        return ProjectInfo(
+            name=name,
+            versions=versions,
+            files=files,
+        )
---
+++ tests/test_repository.py
@@ -0,0 +1,70 @@
+import json
+from pathlib import Path
+
+import pytest
+
+import micropip.repository as repository
+
+
+@pytest.mark.parametrize("name", ["black", "pytest", "snowballstemmer"])
+def test_project_info_from_json(name):
+    test_templates_dir = Path(__name__).parent / "test_data" / "pypi_response"
+
+    test_file = test_templates_dir / f"{name}_json.json"
+    test_data = json.loads(test_file.read_text())
+
+    index = repository.ProjectInfo.from_json_api(test_data)
+    assert index.name == name
+    assert index.versions is not None
+    assert index.files
+
+    for file in index.files:
+        assert file.version in index.versions
+        assert file.filename in file.url
+        assert len(file.sha256) == 64
+
+
+@pytest.mark.parametrize("name", ["black", "pytest", "snowballstemmer"])
+def test_project_info_from_simple_json(name):
+    test_templates_dir = Path(__name__).parent / "test_data" / "pypi_response"
+
+    test_file = test_templates_dir / f"{name}_simple.json"
+    test_data = json.loads(test_file.read_text())
+
+    index = repository.ProjectInfo.from_simple_api(test_data)
+    assert index.name == name
+    assert index.versions is not None
+    assert index.files
+
+    for file in index.files:
+        assert file.version in index.versions
+        assert file.filename in file.url
+        assert len(file.sha256) == 64
+
+
+@pytest.mark.parametrize("name", ["black", "pytest", "snowballstemmer"])
+def test_project_info_equality(name):
+    # The different ways of parsing the same data should result in the same
+
+    test_templates_dir = Path(__name__).parent / "test_data" / "pypi_response"
+
+    test_file_json = test_templates_dir / f"{name}_json.json"
+    test_file_simple_json = test_templates_dir / f"{name}_simple.json"
+
+    test_data_json = json.loads(test_file_json.read_text())
+    test_data_simple_json = json.loads(test_file_simple_json.read_text())
+
+    index_json = repository.ProjectInfo.from_json_api(test_data_json)
+    index_simple_json = repository.ProjectInfo.from_simple_api(test_data_simple_json)
+
+    assert index_json.name == index_simple_json.name
+    assert index_json.versions == index_simple_json.versions
+
+    for f_json, f_simple_json in zip(
+        index_json.files, index_simple_json.files, strict=True
+    ):
+        assert f_json.filename == f_simple_json.filename
+        assert f_json.url == f_simple_json.url
+        assert f_json.version == f_simple_json.version
+        assert f_json.sha256 == f_simple_json.sha256
+        assert f_json.size == f_simple_json.size
