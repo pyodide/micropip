2dd517598d3c6faf98cb38ee1576b0fc615abf89
Younggun Kim <iam@younggun.kim>
2023-07-06 16:12:44 +0900

    Fixed broken link to FAQ

--- micropip/constants.py
+++ micropip/constants.py
@@ -1,3 +1,3 @@
 FAQ_URLS = {
-    "cant_find_wheel": "https://pyodide.org/en/stable/usage/faq.html#micropip-can-t-find-a-pure-python-wheel"
+    "cant_find_wheel": "https://pyodide.org/en/stable/usage/faq.html#why-can-t-micropip-find-a-pure-python-wheel-for-a-package"
 }
