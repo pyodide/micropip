870a8f9cb7c528d4bc859ef26b2a07c3329f72c2
Hood Chatham <roberthoodchatham@gmail.com>
2025-02-22 15:31:51 +0100

    Fix test

--- tests/test_install.py
+++ tests/test_install.py
@@ -360,7 +360,8 @@ def test_emfs_error(selenium_standalone_micropip):
     async def run_test(selenium):
         import micropip
 
-        await micropip.install("emfs:a-2.0.2-cp312-cp312-pyodide_2024_0_wasm32.whl")
+        with pytest.raises(FileNotFoundError, match="No such file or directory: 'a-2.0.2-cp312-cp312-pyodide_2024_0_wasm32.whl'"):
+            await micropip.install("emfs:a-2.0.2-cp312-cp312-pyodide_2024_0_wasm32.whl")
 
     run_test(selenium_standalone_micropip)
 
