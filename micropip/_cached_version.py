bb243f3c7d483a24103067a991bdf14909413b51
Jeongwon Kim <mickykim4@gmail.com>
2025-08-28 02:35:20 +0900

    Lazily cache string and add helper method to create CachedVersion from Version

--- micropip/_cached_version.py
+++ micropip/_cached_version.py
@@ -2,22 +2,29 @@ from ._vendored.packaging.src.packaging.version import Version
 
 
 class CachedVersion(Version):
-    """This class is a subclass of Version with cached hash and string representations for performance."""
+    """This class is a subclass of Version with a lazily cached string representation for performance."""
 
-    __slots__ = ("_cached_hash", "_cached_str")
+    __slots__ = ("_cached_str",)
 
     def __init__(self, version: str) -> None:
         super().__init__(version)
-
-        # Cache expensive computations
-        self._cached_hash = hash(self._key)
-        self._cached_str = super().__str__()
-
-    def __hash__(self) -> int:
-        return self._cached_hash
+        self._cached_str = None
 
     def __str__(self) -> str:
+        if self._cached_str is None:
+            self._cached_str = super().__str__()
         return self._cached_str
+
+    @classmethod
+    def from_version(cls, version: Version) -> "CachedVersion":
+        """Helper method to create a CachedVersion from an existing Version object."""
+        if isinstance(version, cls):
+            return version
+        instance = cls.__new__(cls)
+        instance._version = version._version
+        instance._key = version._key
+        instance._cached_str = None
+        return instance
 
     def __repr__(self) -> str:
         """A representation of the CachedVersion that shows all internal state.
